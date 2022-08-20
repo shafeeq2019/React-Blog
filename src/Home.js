3b8c14c33bf10cb77081ba1bd0ccdb7b7c282f91
Shafeeq Barram <barram@uni-wuppertal.de>
2022-08-21 00:42:16 +0200

    add example blogs list with its style

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,34 @@
+/*
+onClick = {handleClick()} : involks the function
+onClick = {handleClick} : set a referance to the function
+onClick = {
+	() => {
+		handleClick(var); 
+	} 
+} : using anonymous function to set a reference to a function with param
+ */
+import { useState } from 'react';
 const Home = () => {
-	return ( 
+	const [blogs, setBlogs] = useState([
+		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+	]);
+	return (
 		<div className="content">
-			<h2>Homepage</h2>
+			{
+				blogs.map(b => {
+					return (
+					<div className="blog-preview" key={b.id}>
+						<h2>{b.title}</h2>
+						<p>written by {b.author}</p>
+						</div>
+					)
+				})
+			}
+
 		</div>
-	 );
+	);
 }
- 
+
 export default Home;
