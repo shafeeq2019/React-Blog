ffc6f426bf88515cd4f110f139e0c6489cf3c557
Shafeeq Barram <barram@uni-wuppertal.de>
2022-08-20 15:55:19 +0200

    add navbar & home components with some css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import Navbar from './Navbar.js';
+import Home from './Home.js';
 
 function App() {
   return (
     <div className="App">
-			Hello World
+			<Navbar/>
+			<Home/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+	return ( 
+		<div className="content">
+			<h2>Homepage</h2>
+		</div>
+	 );
+}
+ 
+export default Home;
