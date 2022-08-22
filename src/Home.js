c131ba93fe61a712a4e11faaaee068efd658f8c4
Shafeeq Barram <barram@uni-wuppertal.de>
2022-08-22 14:10:14 +0200

    add db to server data from json-server

--- src/Home.js
+++ src/Home.js
@@ -7,20 +7,34 @@ onClick = {
 	} 
 } : using anonymous function to set a reference to a function with param
  */
-import { useState } from 'react'; //useState hook to provide reactive value with its setter
+import { useState, useEffect } from 'react'; //useState hook to provide reactive value with its setter
 import BlogList from './BlogList';
-const data = require('./data.json');
 
 const Home = () => {
-	const [blogs, setBlogs] = useState(data);
+	const [blogs, setBlogs] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		fetchData().then((data) => {
+			setIsLoading(false);
+			setBlogs(data);
+		})
+	}, [])
+
+	const fetchData = async () => {
+		const res = await fetch('http://localhost:8000/blogs');
+		return res.json();
+	}
 
 	const handleDelete = (id) => {
 		setBlogs(blogs.filter(b => b.id !== id));
 	}
 
+	//condetional templation in React 
 	return (
 		<div className="content">
-			<BlogList blogs={blogs} title={"All Blogs!"} handleDelete={handleDelete}></BlogList>
+			{isLoading && <div>Loading..</div>}
+			{blogs && <BlogList blogs={blogs} title={"All Blogs!"} handleDelete={handleDelete}></BlogList>}
 		</div>
 	);
 }
