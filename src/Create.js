624fafd47e873e9d169b3db04e64392bb1081e7d
Shafeeq Barram <barram@uni-wuppertal.de>
2022-08-29 09:58:42 +0200

    Creating a blog works now

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,56 @@
+import { useState } from "react";
+
+
 const Create = () => {
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+	const [author, setAuthor] = useState('Shafeeq');
+	const [isLoading, setLoading] = useState(false);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const blog = { title, body, author }
+		setLoading(true);
+		await fetch(" http://localhost:8000/blogs", {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(blog)
+		}).then(() => {
+			setLoading(false);
+			console.log("New Blog added");
+		});
+	}
+
+
 	return (
 		<div className="create">
 			<h2>Add a new blog</h2>
+			<form onSubmit={handleSubmit}>
+				<label>Blog title:</label>
+				<input
+					type="text"
+					required
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<label>Blog body:</label>
+				<textarea
+					required
+					value={body}
+					onChange={(e) => setBody(e.target.value)}
+				></textarea>
+				<label>Blog author</label>
+				<select
+					value={author}
+					onChange={(e) => setAuthor(e.target.value)}
+				>
+					<option value="shafeeq">shafeeq</option>
+					<option value="Yasser">Yasser</option>
+					<option value="Omar">Omar</option>
+				</select>
+				{!isLoading && <button>Add Blog</button>}
+				{isLoading && <button disabled>Adding Blog ..</button>}
+			</form>
 		</div>
 	);
 }
